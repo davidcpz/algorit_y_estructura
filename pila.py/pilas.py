afcc566ca0accdb3188e4b10f360943571904c8c
davidcpz <cdavidp1@hotmail.com>
2024-05-28 23:19:18 -0300

    guardar

---
+++ pila.py/pilas.py
@@ -0,0 +1,22 @@
+class Stack:
+    def __init__(seft):
+        seft._elements=[]
+
+    def push(self,element):
+        self._elements.append(element)
+
+    def pop (self):
+        if len (self._elements)>0:
+            return self._elements.pop()
+        else:
+            return None
+        
+    def on_top(self):
+        if len(self._elements)>0:
+            return self._elements[-1]
+        else:
+            return None
+        
+    def size(self):
+        return len(self._elements)
+    
